0788cd350518125a6a477c854ba9978293754e72
nomorechokedboy <haiduongit@protonmail.com>
2023-07-13 18:12:44 +0700

    fix(ci): attempt to fix deploy #3

--- apps/api/api/index.go
+++ apps/api/api/index.go
@@ -1,32 +1,25 @@
 package handler
 
 import (
-	"api-blog/pkg/entities"
-	"api-blog/src/config"
 	"api-blog/src/server"
-	"api-blog/src/util"
-	"log"
 	"net/http"
 
 	"github.com/gofiber/adaptor/v2"
-	"github.com/minio/minio-go/v7"
-	"github.com/redis/go-redis/v9"
-	"gorm.io/gorm"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	r.RequestURI = r.URL.String()
 
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	// DB
-	db, err := util.ConnectProstgrest(cfg)
-	if err != nil {
-		panic("Cannot connect Database: %v")
-	}
+	// cfg, err := config.LoadConfig()
+	// if err != nil {
+	// 	log.Fatalf("Failed to load config: %v", err)
+	// }
+	//
+	// // DB
+	// db, err := util.ConnectProstgrest(cfg)
+	// if err != nil {
+	// 	panic("Cannot connect Database: %v")
+	// }
 	// sqlDB, err := db.DB()
 	// if err != nil {
 	// 	log.Panic("Can't get db", err)
@@ -36,41 +29,41 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// sqlDB.SetMaxOpenConns(100)
 	// sqlDB.SetConnMaxLifetime(time.Hour)
 
-	if err = db.
-		AutoMigrate(
-			&entities.User{},
-			// &entities.Post{},
-			// &entities.Slug{},
-			// &entities.Comment{},
-			// &entities.Reaction{},
-			// &notificationEntities.NotificationObject{},
-			// &notificationEntities.Notification{},
-			// &notificationEntities.NotificationChange{},
-		); err != nil {
-		log.Panic("failed to migrate database: ", err)
-	}
-	// Minio
-	minioClient, err := util.ConnectMinio(cfg)
-	if err != nil {
-		panic("Fail to load Minio")
-	}
+	// if err = db.
+	// 	AutoMigrate(
+	// 		&entities.User{},
+	// 		// &entities.Post{},
+	// 		// &entities.Slug{},
+	// 		// &entities.Comment{},
+	// 		// &entities.Reaction{},
+	// 		// &notificationEntities.NotificationObject{},
+	// 		// &notificationEntities.Notification{},
+	// 		// &notificationEntities.NotificationChange{},
+	// 	); err != nil {
+	// 	log.Panic("failed to migrate database: ", err)
+	// }
+	// // Minio
+	// minioClient, err := util.ConnectMinio(cfg)
+	// if err != nil {
+	// 	panic("Fail to load Minio")
+	// }
+	//
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     cfg.Redis.URI,
+	// 	Password: cfg.Redis.Password,
+	// 	DB:       cfg.Redis.DB,
+	// })
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     cfg.Redis.URI,
-		Password: cfg.Redis.Password,
-		DB:       cfg.Redis.DB,
-	})
-
-	handler(cfg, db, minioClient, rdb).ServeHTTP(w, r)
+	handler().ServeHTTP(w, r)
 }
 
 func handler(
-	config *config.Config,
-	db *gorm.DB,
-	minioClient *minio.Client,
-	rdb *redis.Client,
+// config *config.Config,
+// db *gorm.DB,
+// minioClient *minio.Client,
+// rdb *redis.Client,
 ) http.HandlerFunc {
-	app := server.New(config, db, minioClient, rdb)
+	app := server.New()
 
 	return adaptor.FiberApp(app)
 }
